43a75353893238ec06872eaddab7a44fb51ac62f
yi-ren-28 <yi_ren@berkeley.edu>
2026-08-19 14:14:06 -0700

    Add comments to newton.py

--- newton.py
+++ newton.py
@@ -1,12 +1,19 @@
 def f(x):
+    """Define the orginal function"""
     return x + 1
+
+
 def first_derivative(func, x):
+    """Define the first derivative of function f(x)"""
     f_x = (f(x + epsilon) - f(x)) / epsilon
     return f_x
 
+
 def second_derivative(func, x):
-    f_xx = (first_derivative(func, x + epsilon) - first_derivative(func, x) )/ epsilon
+    """Define the second derivative of function f(x)"""
+    f_xx = (first_derivative(func, x + epsilon) - first_derivative(func, x)) / epsilon
     return f_xx
+
 
 epsilon = 1e-5
 second_derivative(f, 2)
